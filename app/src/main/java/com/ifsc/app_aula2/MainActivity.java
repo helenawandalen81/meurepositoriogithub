23fb8673161e0abbe25ec51c8229632682e8569e
helena <xx@ex.com>
2026-08-17 21:58:46 -0300

    sorteio em um intervalo definifo pelo usuario

--- app/src/main/java/com/ifsc/app_aula2/MainActivity.java
+++ app/src/main/java/com/ifsc/app_aula2/MainActivity.java
@@ -2,14 +2,17 @@ package com.ifsc.app_aula2;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     TextView textView;
-    int contador = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +21,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button b = findViewById(R.id.button);
         TextView tv = findViewById(R.id.tv);
+        EditText edmin,edmax;
+        edmin = findViewById(R.id.edMin);
+        edmax = findViewById(R.id.edMax);
 
-        tv.setText(Integer.toString(contador));
+        tv.setText("Número Aleatório");
 
         b.setOnClickListener(view -> {
-            contador++;
-            tv.setText(Integer.toString(contador));
+            String smin = edmin.getText().toString();
+            String smax = edmax.getText().toString();
+
+            if(smin.isEmpty()){
+                edmin.setError("Informe um Número Inteiro!");
+                return;
+            }
+
+            if(smax.isEmpty()){
+                edmax.setError("Informe um Número Inteiro!");
+                return;
+            }
+
+            int min = Integer.parseInt(edmin.getText().toString());
+            int max = Integer.parseInt(edmax.getText().toString());
+
+            if(min>max){
+                Toast.makeText(this, "O valor Minimo não pode ser maior do que o valor Maximo!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            Random random = new Random();
+            int randomN = random.nextInt(min,max);
+            tv.setText(Integer.toString(randomN));
         });
     }
 }
