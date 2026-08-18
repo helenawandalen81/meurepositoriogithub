569173d481f3c35835910703f42d14409bf801b9
helena <xx@ex.com>
2026-08-17 21:09:48 -0300

    contador

--- app/src/main/java/com/ifsc/app_aula2/MainActivity.java
+++ app/src/main/java/com/ifsc/app_aula2/MainActivity.java
@@ -1,24 +1,29 @@
 package com.ifsc.app_aula2;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    int contador = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button b = findViewById(R.id.button);
+        TextView tv = findViewById(R.id.tv);
+
+        tv.setText(Integer.toString(contador));
+
+        b.setOnClickListener(view -> {
+            contador++;
+            tv.setText(Integer.toString(contador));
         });
     }
 }
